edc0a4c49c2e223785798b1847d94e355ef6b180
plodixrv-hub <plodixrv@gmail.com>
2026-05-01 11:56:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def on_message(message):
     # --- Sistema de IA ---
     if message.channel.id == CANAL_IA:
         ahora = time.time()
-        if ahora - ultimo_mensaje_global_ia >= 60:
+        if ahora - ultimo_mensaje_global_ia >= 10:
             ultimo_mensaje_global_ia = ahora
             if message.author.id in USUARIOS_BONITOS:
                 personalidad_usar = PERSONALIDAD_BONITA
